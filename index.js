ab41462ea2ca14f3b1c4ea80baf8bdfa0c268508
vang007 <https://github.com/vang007>
2021-11-23 02:57:01 -0600

    almost done

--- index.js
+++ index.js
@@ -1,68 +1,142 @@
 //packages needed for this application
-const fs = require('fs');
 const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
+const writeFileAsync = util.promisify(fs.writeFile);
 // read me questions--user input
-const questions = [
-    {
-        type: "input",
-        message: "Title of Your Repository",
-        name: "title"
-    },
-    {
-        type: "input",
-        message: "Description",
-        name: "info"
-    },
-    {
-        type: "input",
-        message: "Table of Contents",
-        name: "contents"
-    },
-    {
-        type: "input",
-        message: "Installation Instructions",
-        name: "install"
-    },
-    {
-        type: "input",
-        message: "Usage Information",
-        name: "usage"
-    },
-    {
-        type: "input",
-        message: "License",
-        name: "Choose from the following: ",
-        choices: [
-            "apache-2.0",
-            " ",
-            " ",
-            " ",
-            " ",
-        ]
-    },
-    {
-        type: "input",
-        message: "Author/ Contributors",
-        name: "writer(s)"
-    },
-    {
-        type: "input",
-        message: "Tests",
-        name: "check"
-    }
-],
+const questions = () =>
+    inquirer.prompt([{
+            type: 'input',
+            message: 'Project Title',
+            name: 'title'
+                //     validate: (value) => {
+                //         if (value) {
+                //             return true
+                //         } else {
+                //             return 'i need a value to continue'
+                //         }
+                //     },
+                // },
+                // {
+                //     type: "input",
+                //     message: "Table of Contents",
+                //     name: "contents",
+                //     // validate: (value) => {
+                //     //     if (value) {
+                //     //         return true
+                //     //     } else {
+                //     //         return 'i need a value to continue'
+                //     //     }
+                //     },
+        },
+        {
+            type: 'input',
+            message: 'Description',
+            name: 'info'
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                // }
+                // },
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions',
+            name: 'install'
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                //     }
+                // },
+        },
+        {
+            type: 'input',
+            message: 'Usage Information',
+            name: 'usage'
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                //     }
+                // },
+        },
+        {
+            //small list of licenses
+            type: 'list',
+            message: 'License: Choose from the following: ',
+            name: 'license',
+            choices: ['apache-2.0', 'The MIT License', 'GNU license', 'The Unlicense', 'N/A']
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                //     }
+                // },
+        },
+        {
+            type: 'input',
+            message: 'Author/ Contributors',
+            name: 'contributions'
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                //     }
+                // },
+        },
+        {
+            type: 'input',
+            message: 'Tests',
+            name: 'check'
+                // validate: (value) => {
+                //     if (value) {
+                //         return true
+                //     } else {
+                //         return 'i need a value to continue'
+                //     }
+                // },
+        },
 
-    //function to write README file
-function writeToFile(file, data) {
-    fs.writeToFile(file, function (err) {
-        if (err) throw console.error(console.log("pass"));
-    });
+        function generateMD(data) {
+            return `# ${data.title}
+    $(badge)
+    ${data.description}
+    ## Table of Contents:
+    * [Installation](#installation)
+    * [Usage](#usage)
+    * [License](#license)
+    * [Contributions](#contributions)
+    * [Tests],(#tests)
+    * [Questions](#questions)
+    ### Installation:
+    To get the correct dependencies, open the console and run the following: 
+    \`\`\`${data.installations}\`\`\`
+    ### Usage:
+    ${data.usage}
+    ### License:
+    Licensed by: ${data.license}
+    ### Contributing:
+    ${(data.contribute, data.author)}
+    ### Tests:
+    To test, open console and run the following:
+    \`\`\`${data.tests}\`\`\`
+    ### Questions:
+    Having trouble? Feel free to contact me, ${data.author} via ${data.email}
+    or if you're just feeling the need see my other projects 
+    I have on my Github, simply click here:(https://github.com/${data.username}
+    `;
+        },
 
-    // TODO: Create a function to initialize app
-    function init() { }
-
-    // Function call to initialize app
-    init();
-    fs.appendFileSync('app.js')
-}
+        questions()
+        .then((data) => writeFileAsync('generatedREADME.md', generateMD(data)))
+        .then(() => console.log('README.md has been successfully generated!'))
+        .catcher((err) => console.error(err))
+    ]);
